ed9d65a5f4e611b41966e951b295e566038e34dd
Federico Tartarini <federicotartarini@proton.me>
2026-09-15 17:40:52 +1000

    Bump version: 4.5.0rc1 → 4.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = "pythermalcomfort"
 year = "2025"
 author = "Federico Tartarini"
 project_copyright = f"{year}, {author}"
-version = release = "4.5.0rc1"
+version = release = "4.5.0"
 
 autodoc_typehints = "none"
 
